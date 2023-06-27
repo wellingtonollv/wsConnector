c54316ee022dac88e3d6cec0bf055df1e896aba3
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2023-06-27 10:12:51 -0300

    :recycle: refactor: export oculus func

--- oculus.js
+++ oculus.js
@@ -1,24 +1,21 @@
 const adb = require("adbkit");
 const client = adb.createClient();
+const env  = require('dotenv').config();
 
 async function setOculusBrightness(brightnessValue) {
+  // 0 - 255 (0 is off, 255 is full brightness)
   try {
     // Find the device ID
     const devices = await client.listDevices();
-    const device = devices.find((d) => d.id.includes(process.env.DEVICE_ID));
-
+    const device = devices.find((device) => device.id.includes(process.env.OCULUS_DEVICE_ID));
     // Set the brightness
     await client.shell(device.id, `settings put system screen_brightness ${brightnessValue}`);
+    console.log(`Oculus brightness set to ${brightnessValue}`);
   } catch (error) {
     console.error(`Failed to set Oculus brightness: ${error}`);
   }
 }
 
-exports.handler = async (event) => {
-  const brightnessValue = event.brightnessValue;
-  await setOculusBrightness(brightnessValue);
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Brightness set successfully"),
-  };
+module.exports = {
+  setOculusBrightness
 };
